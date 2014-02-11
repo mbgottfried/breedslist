5b92e58ab36041a284008a223ea7cdd4d17700e0
shagmbg <michael.b.gottfried@gmail.com>
2014-02-11 15:25:52 -0500

    Add Amazon S3 for paperclip image uploads to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,13 @@ Breedslist::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.default_url_options = { :host => 'http://arcane-caverns-6730.herokuapp.com/' }
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
