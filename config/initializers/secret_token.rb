83711b88c4d79b5ac1e0f4b9c47c4f127bdf8694
shagmbg <michael.b.gottfried@gmail.com>
2014-02-11 11:37:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Breedslist::Application.config.secret_key_base = '5dd0999e71e4b36a7d09edda39cb50bacc39298a28ba7829332764d5994acb24e64923db36ed796ed4cd5be4a8c56268e10a35feb9d4cec62d00a52885196b67'
