a16a071ac2ae3810cdd50ec0d8b5daba4827bddb
shagmbg <michael.b.gottfried@gmail.com>
2014-02-11 12:15:34 -0500

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Breedslist::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'http://arcane-caverns-6730.herokuapp.com/' }
 end
